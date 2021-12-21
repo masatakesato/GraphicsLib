fb2c325b44307b621e77875d1f038eaeb34abbd7
masatake1115 <masatake1115@hotmail.co.jp>
2021-12-21 18:14:58 +0900

    Added TextureLoader class

---
+++ graphics/vulkan/utils/TextureLoader.cpp
@@ -0,0 +1,49 @@
+﻿#include	"TextureLoader.h"
+
+
+namespace vk
+{
+
+	TextureLoader::TextureLoader()
+		: FreeImageLoader()
+	{
+
+	}
+
+
+
+	TextureLoader::~TextureLoader()
+	{
+
+	}
+
+
+
+	bool TextureLoader::Load( GraphicsDevice& device, Texture& tex, const tstring& filepath, bool bFloat, bool bFlip )
+	{
+		auto dib = FreeImageLoader::Load( filepath, bFloat, bFlip );
+
+		auto texWidth = FreeImage_GetWidth( dib );
+		auto texHeight = FreeImage_GetHeight( dib );
+		auto bpp = FreeImage_GetBPP( dib );
+		
+		VkDeviceSize imageSize = texWidth * texHeight * 4;// * bpp;
+
+		tex.Init( device, texWidth, texHeight, VK_FORMAT_B8G8R8A8_SRGB, true );
+		tex.UploadData( FreeImage_GetBits( dib ), imageSize );
+
+		FreeImage_Unload( dib );
+
+		return true;
+	}
+
+
+
+	bool TextureLoader::Save( const Texture& tex, const tstring& filepath, bool bFlip )
+	{
+
+		return false;
+	}
+
+
+}// end of namespace vk
---
+++ graphics/vulkan/utils/TextureLoader.h
@@ -0,0 +1,28 @@
+﻿#ifndef TEXTURE_LOADER_H
+#define	TEXTURE_LOADER_H
+
+#include	<oreore/images/FreeImageLoader.h>
+#include	<graphics/vulkan/core/Texture.h>
+
+
+
+namespace vk
+{
+
+	class TextureLoader : public OreOreLib::FreeImageLoader
+	{
+	public:
+
+		TextureLoader();
+		~TextureLoader();
+
+		bool Load( GraphicsDevice& device, Texture& tex, const tstring& filepath, bool bFloat, bool bFlip );
+		bool Save( const Texture& tex, const tstring& filepath, bool bFlip );
+
+	};
+
+
+}// end of namespace vk
+
+
+#endif // !TEXTURE_LOADER_H
