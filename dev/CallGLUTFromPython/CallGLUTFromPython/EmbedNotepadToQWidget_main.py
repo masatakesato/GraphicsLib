d331237e502db5c340924cbf444ad84f78b072f2
masatake1115 <masatake1115@hotmail.co.jp>
2022-03-27 18:32:12 +0900

    global key event detection using pyHook

--- dev/CallGLUTFromPython/CallGLUTFromPython/EmbedNotepadToQWidget_main.py
+++ dev/CallGLUTFromPython/CallGLUTFromPython/EmbedNotepadToQWidget_main.py
@@ -1,10 +1,14 @@
 # http://timgolden.me.uk/python/win32_how_do_i/find-the-window-for-my-subprocess.html
 
+from ctypes.wintypes import *
+import ctypes.wintypes
+
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 
 # pywin32 module required. 
+import win32api
 import win32con
 import win32gui
 import win32process
@@ -14,14 +18,74 @@ import subprocess
 import time
 
 
+
+
+class MyNativeEventFilter( QAbstractNativeEventFilter ):
+
+	def __init__( self ):
+		super(MyNativeEventFilter, self).__init__()
+
+
+	def nativeEventFilter( self, eventType, message ):
+
+		if( eventType == "windows_generic_MSG" ):
+			msg = ctypes.wintypes.MSG.from_address( message.__int__() )
+
+			print( msg.message )
+
+
+			if( msg.message == win32con.WM_PARENTNOTIFY ):
+				print( "WM_PARENTNOTIFY" )
+
+			elif( msg.message == win32con.WM_MOUSEACTIVATE ):
+				print( "WM_MOUSEACTIVATE" )
+
+
+
+			elif( msg.message == win32con.WM_KEYDOWN ):
+				print( "WM_KEYDOWN" )
+
+			elif( msg.message == win32con.WM_KEYUP ):
+				print( "WM_KEYUP" )
+
+			elif( msg.message == win32con.WM_LBUTTONDOWN ):
+				print( "WM_LBUTTONDOWN" )
+
+		return False, 0
+
+
+
+
+
+class MINMAXINFO(ctypes.Structure):
+	_fields_ = [
+		("ptReserved",      POINT),
+		("ptMaxSize",       POINT),
+		("ptMaxPosition",   POINT),
+		("ptMinTrackSize",  POINT),
+		("ptMaxTrackSize",  POINT),
+	]
+
+
 class MyWidget(QWidget):
 
 	def __init__( self ):
 		super( MyWidget, self ).__init__()
-		self.setEnabled( True)
-		self.setFocusPolicy( Qt.StrongFocus )
+		#self.setFocusPolicy( Qt.NoFocus )
 		self.setLayout( QVBoxLayout() ) 
-		self.setGeometry(0, 0, 400,800)
+		self.setGeometry(100, 100, 400,800)
+		#self.resize( 400, 400)
+		self.myEventFilter = MyNativeEventFilter()
+
+		self.window_widget = None
+
+
+	def BindHwnd( self, hwnd ):
+		self.window_widget = QWidget.createWindowContainer( window_wrapper, self )
+		#self.window_widget.installEventFilter( self )
+		self.layout().addWidget( self.window_widget )
+		ctypes.windll.user32.RegisterHotKey()
+
 
 
 	def mousePressEvent( self, a0 ):
@@ -34,50 +98,148 @@ class MyWidget(QWidget):
 		return super().mouseReleaseEvent(a0)
 
 
+	#def event( self, event ):
+	#	print( "MyWidget::event" )
+	#	return super().event( event )
+
+
 	def keyPressEvent( self, event ):
 		print( "MyWidget::keyPressEvent" )
 		return super().keyPressEvent( event )
 
 
 
+	def GET_X_PARAM( self, param ):
+		return param & 0xffff
 
-def get_hwnds_for_pid (pid):
-	def callback (hwnd, hwnds):
-		if win32gui.IsWindowVisible (hwnd) and win32gui.IsWindowEnabled (hwnd):
-			_, found_pid = win32process.GetWindowThreadProcessId (hwnd)
-			if found_pid == pid:
-				hwnds.append (hwnd)
+
+	def GET_Y_PARAM( self, param ):
+		return param >> 16
+
+
+	# https://jpdebug.com/p/365848
+	# https://gitpress.io/u/1155/pyqt-example-NativeEvent
+	#def nativeEvent( self, eventType, message ):
+	#	retval, result = super(MyWidget, self).nativeEvent(eventType, message)
+
+	#	print( eventType )
+
+	#	if( eventType == "windows_generic_MSG" ):
+	#		msg = ctypes.wintypes.MSG.from_address( message.__int__() )
+	#		print( msg.lParam )
+	#		x = self.GET_X_PARAM( msg.lParam ) #win32api.LOWORD(msg.lParam)# - self.frameGeometry().x()
+	#		y = self.GET_Y_PARAM( msg.lParam ) #win32api.HIWORD(msg.lParam) - self.frameGeometry().y()
+	#		#print( x, y )
+
+	#		# https://mcz-xoxo.cocolog-nifty.com/blog/2015/01/post-5960.html
+	#		#wparam = msg.wParam
+	#		#print( wparam )
+	#		#print( msg.message==win32con.WM_KEYDOWN )
+
+
+	#		# nativeEventFilter
+	#		# https://qtcoder.hatenadiary.org/entry/20140626/1403794191 これ？
+
+	#	#	if( self.childAt(x, y) != None ):
+	#	#		return retval, result
+	#	#	
+	#	#	if( msg.message == win32con.WM_NCCALCSIZE ):# ?
+	#	#		return True, 0
+
+	#	#	if( msg.message == win32con.WM_GETMINMAXINFO ):# window position or size has been changed
+	#	#		info = ctypes.cast( msg.lParam, ctypes.POINTER(MINMAXINFO) ).contents
+	#	#		# modify maximized window size to available size
+	#	#		#info.ptMaxSize.x = self.
+	#	#		#info.ptMaxSize.y = self.
+	#	#		info.ptMaxPosition.x = 0
+	#	#		info.ptMaxPosition.y = 0
+
+	#	#print( "MyWidget::nativeEvent: ", message  )
+
+	#	return retval, result
+
+
+def get_hwnds_for_pid( pid ):
+
+	def callback( hwnd, hwnds ):
+		if( win32gui.IsWindowVisible(hwnd) and win32gui.IsWindowEnabled(hwnd) ):
+			_, found_pid = win32process.GetWindowThreadProcessId( hwnd )
+			if( found_pid == pid ):
+				hwnds.append( hwnd )
 		return True
 
 	hwnds = []
-	win32gui.EnumWindows (callback, hwnds)
+	win32gui.EnumWindows( callback, hwnds )
 	return hwnds
 
+
+
+def get_child_hwnds( hwnd ):
+
+	#def callback( handle, childlist ):
+	#	childlist.append( handle )
+	#	return True
+
+	def callback( hwnd, hwnds ):
+		if( win32gui.IsWindowVisible(hwnd) and win32gui.IsWindowEnabled(hwnd) ):
+			hwnds.append( hwnd )
+		return True
+
+	child_hwnds = []
+	win32gui.EnumChildWindows( hwnd, callback, child_hwnds )
+	return child_hwnds
+
+
+
+
+
+
+
+
+# https://bbs.huaweicloud.com/blogs/106187
+# https://gitlab.com/Gluttton/SOS/tree/master/38096428/solution
+
+
+# https://www.qtcentre.org/threads/67495-native-event-of-child-window/page2
+# TODO: QWindowのmousePressed, keyPressedEventあたりを実装する?
+
+# http://bttb.s1.valueserver.jp/wordpress/blog/2017/09/30/python%E3%81%A7%E5%AD%90%E3%82%A6%E3%82%A4%E3%83%B3%E3%83%89%E3%82%A6%E3%83%8F%E3%83%B3%E3%83%89%E3%83%AB%E3%82%92%E5%8F%96%E5%BE%97%E3%81%99%E3%82%8B%E6%96%B9%E6%B3%95enumchildwindows/
+# Pythonで子ウインドウハンドルを取得する方法.
+
+# https://code.tiblab.net/python/pyhook
+# pyHookで引っ掛ける?
 
 if __name__ == "__main__":
 
 	app = QApplication( sys.argv )
 
+	w = MyWidget()
+	app.installNativeEventFilter( w.myEventFilter )
+
 	notepad = subprocess.Popen( [r"C:\\Windows\\system32\\notepad.exe"] )#[r"C:\\Windows\\System32\\mspaint.exe"] ) #
 
 	time.sleep (0.5)
 
+	parent_window_handles = get_hwnds_for_pid( notepad.pid )
 
-	w = MyWidget()
-#TODO: runProgramからhwnd引っ張り出す
-	for hwnd in get_hwnds_for_pid( notepad.pid ):
-		window_wrapper = QWindow.fromWinId( hwnd )
-		window_widget = QWidget.createWindowContainer( window_wrapper )
-		#window_widget.show()
-		#break
+	child_handles = []
+	child_handles = get_child_hwnds( parent_window_handles[0] )
+	print( child_handles )# 0:テキストフィールド, 1:ステータスバー
 
-		w.layout().addWidget( window_widget )
-		#w.createWindowContainer( window_wrapper, w )
-		#w.setGeometry(0, 0, 800, 600)
-		w.show()
-		break
+	#c_handle = ctypes.windll.user32.FindWindowExW( parent_window_handles[0], 0, 'Edit', 0 )
+	#print("子ウインドウハンドル: ", c_handle )
 
-		
+
+	#for hwnd in child_handles:#parent_window_handles:
+	#	window_wrapper = QWindow.fromWinId( hwnd )
+	#	window_widget = QWidget.createWindowContainer( window_wrapper )
+	#	window_widget.show()
+
+	window_wrapper = QWindow.fromWinId( parent_window_handles[0] )#child_handles[0] )#
+	w.BindHwnd( window_wrapper )
+	w.setGeometry(0, 0, 800, 600)
+	w.show()
+
 	sys.exit( app.exec_() )
 
 
---
+++ dev/CallGLUTFromPython/CallGLUTFromPython/test.py
@@ -0,0 +1,297 @@
+#import os
+#import time
+#import win32.win32gui as win32gui
+#import sys
+#import os
+#from PyQt5.QtCore import *
+#from PyQt5.QtWidgets import *
+#from PyQt5.QtGui import *
+#import subprocess
+
+#import win32con
+#import win32gui
+#import win32process
+
+
+##class MainWindow(QtWidgets.QMainWindow):
+##    def __init__(self):
+##        super(MainWindow, self).__init__()
+
+##        widget = QtWidgets.QWidget()
+##        layout = QtWidgets.QGridLayout(widget)
+##        self.setCentralWidget(widget)
+
+##        exePath = "C:\\Windows\\system32\\calc.exe"
+##        # subprocess.Popen(exePath)
+##        os.system('"%s"' % exePath)
+##        hwnd = win32gui.FindWindow(0, 'Calculator')
+##        time.sleep(0.05)
+##        print(hwnd)
+##        self.window = QtGui.QWindow.fromWinId(hwnd)    
+##        self.windowcontainer = self.createWindowContainer(self.window, widget)
+
+##        layout.addWidget(self.windowcontainer, 0, 0)
+
+##if __name__ == '__main__':
+
+##    app = QtWidgets.QApplication(sys.argv)
+##    app.setStyle("fusion")
+##    form = MainWindow()
+##    form.setWindowTitle('Embed')
+##    form.setGeometry(100, 100, 400, 600)
+##    form.show()
+##    sys.exit(app.exec_())
+
+
+## Second Solution
+##class MainWindow(QtWidgets.QMainWindow):
+##     def __init__(self):
+##         super(MainWindow, self).__init__()
+
+##         widget = QtWidgets.QWidget()
+##         layout = QtWidgets.QGridLayout(widget)
+##         self.setCentralWidget(widget)
+
+##         exePath = "C:\Windows\system32\calc.exe"
+##         subprocess.Popen(exePath)
+##         # os.system('"%s"' % exePath)
+##         hwnd = win32gui.FindWindow(0, 'Calculator')
+##         time.sleep(0.05)
+##         window = QtGui.QWindow.fromWinId(hwnd)
+##         self.createWindowContainer(window,self)
+##         # self.setGeometry(500,500,450,400)
+##         self.setWindowTitle("File Dialog")
+##         self.show()
+
+##if __name__ == '__main__':
+##     app = QtWidgets.QApplication(sys.argv)
+##     app.setStyle("fusion")
+##     form = MainWindow()
+##     form.setWindowTitle('My Embedded Calculator')
+##     form.setGeometry(600, 600, 600, 600)
+##     form.show()
+##     sys.exit(app.exec_())
+
+
+#def get_hwnds_for_pid (pid):
+#	def callback (hwnd, hwnds):
+#		if win32gui.IsWindowVisible (hwnd) and win32gui.IsWindowEnabled (hwnd):
+#			_, found_pid = win32process.GetWindowThreadProcessId (hwnd)
+#			if found_pid == pid:
+#				hwnds.append (hwnd)
+#		return True
+
+#	hwnds = []
+#	win32gui.EnumWindows (callback, hwnds)
+#	return hwnds
+
+
+## Solution 3
+#class MainWindow( QMainWindow ):
+
+#	def __init__(self):
+#		super(MainWindow, self).__init__()
+
+#		# create a process
+#		exePath = "C:\\Windows\\system32\\notepad.exe"
+
+#		notepad = subprocess.Popen( [exePath] )
+
+#		#subprocess.Popen(exePath)
+#		# os.system(exePath)
+#		#hwnd = win32gui.FindWindow(0, "" )
+#		#print(hwnd)
+#		time.sleep(0.05)
+
+#		#window = QtGui.QWindow.fromWinId(hwnd)
+#		#self.createWindowContainer(window, self)
+#		self.setGeometry(0, 0, 502, 752)
+#		self.setWindowTitle('Embedded Window')
+
+#		hwnds = get_hwnds_for_pid( notepad.pid )
+
+#		for hwnd in get_hwnds_for_pid( notepad.pid ):
+#			window_wrapper = QWindow.fromWinId( hwnd )
+#			self.createWindowContainer( window_wrapper, self )
+#			#window_widget.show()
+
+#		self.activateWindow()
+
+
+#if __name__ == '__main__':
+#	app = QApplication(sys.argv)
+#	form = MainWindow()
+#	form.setWindowTitle(' Embedded ')
+#	form.setGeometry(100, 100, 400, 600)
+#	form.show()
+#	sys.exit(app.exec_())
+
+
+
+
+# https://code.tiblab.net/python/pyhook
+
+import sys
+import threading
+
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+import pythoncom, pyWinhook, ctypes
+
+
+
+class Window( QFrame ):
+
+	def __init__(self, parent=None):
+		super(Window, self).__init__(parent)
+
+		self.hm = pyWinhook.HookManager()
+
+		self.label = QLabel('hook:',self)
+
+		self.timer = QTimer(self)
+		self.timer.timeout.connect( self.hookThread ) #self.hook)
+		self.timer.start()
+
+
+	def closeEvent( self,event ):
+		print( 'closeEvent' )
+		ctypes.windll.user32.PostQuitMessage(0)
+
+
+	#def setTimer( self ):
+	#	print('setTimer')
+	#	self.timer = QTimer(self)
+	#	self.timer.timeout.connect(self.hook)
+	#	self.timer.start()
+
+
+	def hook( self ):
+		print( 'hook' )
+		self.hm.KeyDown = self.hookEvent
+		self.hm.HookKeyboard()
+		pythoncom.PumpMessages()
+		#self.label.setText(self.label.text())#for update
+
+
+	def hookEvent( self, event ):
+		print('hookEvent')
+		self.label.setText( event.Key )#self.label.text() + event.Key )
+		ctypes.windll.user32.PostQuitMessage(0)
+		return True
+
+
+	def hookThread( self ):
+		print( 'hookThread' )
+		self.hm.KeyDown = self.hookEventThread
+		self.hm.HookKeyboard()
+		pythoncom.PumpMessages()
+		#self.label.setText(self.label.text())#for update
+
+	def hookEvent2( self, event ):
+		print('hookEvent2')
+		self.label.setText( event.Key )#self.label.text() + event.Key )
+		#ctypes.windll.user32.PostQuitMessage(0)
+
+	def hookEventThread( self, event ):
+		print('hookEventThread')
+		th = threading.Thread( target=self.hookEvent2, args=(event,))
+		th.daedaemon = True
+		th.start()
+		return True
+
+
+
+if __name__ == '__main__':
+	app = QApplication( sys.argv )
+	window = Window()
+	window.show()
+	sys.exit( app.exec_() )
+
+
+
+
+
+
+
+## https://tousu.in/qa/?qa=1087440/
+## キー大量に推し過ぎるとクラッシュする
+
+## https://stackoverflow.com/questions/14449956/python-crash-with-pyhook-hookmanager-on-keydown-ctrlc
+
+#import threading
+#import pythoncom, pyWinhook, ctypes
+
+#def OnKeyboardEvent(event):
+#	print( 'MessageName:',event.MessageName )
+#	print( 'Message:',event.Message )
+#	print( 'Time:',event.Time )
+#	print( 'Window:',event.Window )
+#	print( 'WindowName:',event.WindowName )
+#	print( 'Ascii:', event.Ascii, chr(event.Ascii) )
+#	print( 'Key:', event.Key )
+#	print( 'KeyID:', event.KeyID )
+#	print( 'ScanCode:', event.ScanCode )
+#	print( 'Extended:', event.Extended )
+#	print( 'Injected:', event.Injected )
+#	print( 'Alt', event.Alt )
+#	print( 'Transition', event.Transition )
+#	print( '---' )
+
+#	#ctypes.windll.user32.PostQuitMessage(0)
+
+#	# return True to pass the event to other handlers
+#	return True
+
+
+#def OnKeyboardEvent2(event):
+#	print( 'MessageName:',event.MessageName )
+#	print( 'Message:',event.Message )
+#	print( 'Time:',event.Time )
+#	print( 'Window:',event.Window )
+#	print( 'WindowName:',event.WindowName )
+#	print( 'Ascii:', event.Ascii, chr(event.Ascii) )
+#	print( 'Key:', event.Key )
+#	print( 'KeyID:', event.KeyID )
+#	print( 'ScanCode:', event.ScanCode )
+#	print( 'Extended:', event.Extended )
+#	print( 'Injected:', event.Injected )
+#	print( 'Alt', event.Alt )
+#	print( 'Transition', event.Transition )
+#	print( '++++++++++++++++' )
+
+#	#ctypes.windll.user32.PostQuitMessage(0)
+#	#pythoncom.PumpMessages()
+
+
+#def OnKeyboardEventThread( event ):
+#	th = threading.Thread( target=OnKeyboardEvent2, args=(event,)).start()
+#	print('*********************')
+#	return True
+
+
+#import time
+
+#if __name__=="__main__":
+
+#	hm = pyWinhook.HookManager()
+#	hm.KeyDown = OnKeyboardEvent#OnKeyboardEventThread#
+#	hm.HookKeyboard()
+#	pythoncom.PumpMessages()
+
+#	#while True:
+#	#	try:
+#	#		while True:
+#	#			pythoncom.PumpWaitingMessages()
+#	#	except KeyboardInterrupt:
+#	#		print("TGR$T$#T#T$")
+#	#		pass
+
+
+
+#	print("gfdgfdggfdgfgd")
+
+#	for i in range(10):
+#		time.sleep(0.5)
+#		print("-+++")
