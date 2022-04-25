795540470ade5dcf514c27eed9b5c1183f5e7b77
masatake1115 <masatake1115@hotmail.co.jp>
2022-04-25 12:21:21 +0900

    TODO: keylogger thread

--- dev/CallGLUTFromPython/testKeyLogger/keylogger/listener.py
+++ dev/CallGLUTFromPython/testKeyLogger/keylogger/listener.py
@@ -3,8 +3,8 @@ import ctypes
 from ctypes import *
 from ctypes.wintypes import DWORD
 
-import const as Const
-from event import KeyboardEvent
+from .const import Const
+from .event import KeyboardEvent
 
 
 
@@ -40,42 +40,37 @@ class KeyLogger:
     def __init__( self ):
 
         self.lUser32 = user32
-        self.__m_HHook = None# handle to the hook procedure
-
-        #self.__m_refCallbackFuncs = {}# WH_KEYBOARD_LL, WH_MOUSE_LLだけ? WH_MAX個必要?
+        self.__m_KeyState = ( ctypes.c_char * 256 )()
+        self.__m_KeyHookHandle = None# handle to the hook procedure
+        self.__m_KeyHookFuncPtr = None
         self.__m_CustomEventFuncs = {}# Key: WM_***
 
-        self.__m_KeyState = ( ctypes.c_char * 256 )()
-
-
-
-        pointer = HOOKPROC( self.__KeyboardHookProc )
-        self.__InstallHookProc( pointer )
+        self.__InstallHookProc()
 
 
 
     def __del__( self ):
-        self.__UninstallHookProc()
+        self.__UninstallKeyHookProc()
 
 
 
     def BindKeyDown( self, func ):
-        self.__m_CustomEventFuncs[ WM_KEYDOWN ] = func
-        self.__m_CustomEventFuncs[ WM_SYSKEYDOWN ] = func
+        self.__m_CustomEventFuncs[ Const.WM_KEYDOWN ] = func
+        self.__m_CustomEventFuncs[ Const.WM_SYSKEYDOWN ] = func
 
 
 
     def BindKeyUp( self, func ):
-        self.__m_CustomEventFuncs[ WM_KEYUP ] = func
-        self.__m_CustomEventFuncs[ WM_SYSKEYUP ] = func
+        self.__m_CustomEventFuncs[ Const.WM_KEYUP ] = func
+        self.__m_CustomEventFuncs[ Const.WM_SYSKEYUP ] = func
 
 
 
     def BindKeyChar( self, func ):
-        self.__m_CustomEventFuncs[ WM_CHAR ] = func
-        self.__m_CustomEventFuncs[ WM_DEADCHAR ] = func# dead keys
-        self.__m_CustomEventFuncs[ WM_SYSCHAR ] = func # F10/Alt
-        self.__m_CustomEventFuncs[ WM_SYSDEADCHAR ] = func
+        self.__m_CustomEventFuncs[ Const.WM_CHAR ] = func
+        self.__m_CustomEventFuncs[ Const.WM_DEADCHAR ] = func# dead keys
+        self.__m_CustomEventFuncs[ Const.WM_SYSCHAR ] = func # F10/Alt
+        self.__m_CustomEventFuncs[ Const.WM_SYSDEADCHAR ] = func
 
 
 
@@ -86,21 +81,24 @@ class KeyLogger:
 
 
 
-    def __InstallHookProc( self, pointer ):
+    def __InstallKeyHookProc( self ):
 
         # hinstの入力に注意. https://stackoverflow.com/questions/49898751/setwindowshookex-gives-error-126-module-not-found-when
         # 明示的にやらないと126(module not found)が発生して関数フックできない
         hinst = ctypes.windll.LoadLibrary('user32')._handle
         #hinst = kernel32.GetModuleHandleW( None )
 
-        self.__m_HHook = SetWindowsHookEx(#self.lUser32.SetWindowsHookExA(#
+        self.__m_KeyHookFuncPtr = HOOKPROC( self.__KeyboardHookProc )
+
+        self.__m_KeyHookHandle = SetWindowsHookEx(#self.lUser32.SetWindowsHookExA(#
             win32con.WH_KEYBOARD_LL,
-            pointer,
+            self.__m_KeyHookFuncPtr,
             hinst,
             0
         )
 
-        if( not self.hooked ):
+
+        if( not self.__m_KeyHookHandle ):
             print( "Failed hook procedure installation:", str( kernel32.GetLastError() ) )
             return False
 
@@ -109,12 +107,14 @@ class KeyLogger:
 
 
 
-    def __UninstallHookProc( self ):
-        if( self.__m_HHook is None ):
+    def __UninstallKeyHookProc( self ):
+        print( "Uninstalled hook procedure" )
+        if( self.__m_KeyHookHandle is None ):
             return
 
-        self.lUser32.UnhookWindowsHookEx( self.__m_HHook )
-        self.__m_HHook = None
+        self.lUser32.UnhookWindowsHookEx( self.__m_KeyHookHandle )
+        self.__m_KeyHookHandle = None
+        self.__m_KeyHookFuncPtr = None
 
 
 
@@ -144,13 +144,13 @@ class KeyLogger:
 
         kb = KBDLLHOOKSTRUCT.from_address( lParam )
 
-        event = KeyboardEvent( wParam, kb.vkCode, kb.scanCode, kb.flags, kb.time, hwnd, title )
+        e = KeyboardEvent( wParam, kb.vkCode, kb.scanCode, kb.flags, kb.time, hwnd, title )
         func = self.__m_CustomEventFuncs.get( wParam )
-        result = func( event ) if func else True #self.__m_refCallbackFuncs[ win32con.WH_KEYBOARD_LL ]# KeyEvent
+        result = func( e ) if func else True #self.__m_refCallbackFuncs[ win32con.WH_KEYBOARD_LL ]# KeyEvent
 
         if( result ):
             self.__UpdateKeyState( kb.vkCode, wParam )
-            return user32.CallNextHookEx( self.__m_HHook, nCode, wParam, ctypes.c_longlong(lParam) )# メッセージそのまま通す
+            return user32.CallNextHookEx( self.__m_KeyHookHandle, nCode, wParam, ctypes.c_longlong(lParam) )# メッセージそのまま通す
         else:
             return True# メッセージ伝播をブロックする
 
@@ -197,25 +197,25 @@ class KeyLogger:
 
 	    #(1 > 0) ? True : False
         if( vkey == Const.VK_MENU or vkey == Const.VK_LMENU or vkey == Const.VK_RMENU ):
-            keyboard_state[ vkey ] = 0x80 if down else 0x00
-            keyboard_state[ Const.VK_MENU ] = OR( keyboard_state[ Const.VK_LMENU ], keyboard_state[ Const.Const.VK_RMENU ] )
+            self.__m_KeyState[ vkey ] = 0x80 if down else 0x00
+            self.__m_KeyState[ Const.VK_MENU ] = OR( self.__m_KeyState[ Const.VK_LMENU ], self.__m_KeyState[ Const.VK_RMENU ] )
 
         elif( vkey == Const.VK_SHIFT or vkey == Const.VK_LSHIFT or vkey == Const.VK_RSHIFT):
-            keyboard_state[ vkey ] = 0x80 if down else 0x00
-            keyboard_state[ Const.VK_SHIFT ] = OR( keyboard_state[ Const.VK_LSHIFT ], keyboard_state[ Const.VK_RSHIFT ] )
+            self.__m_KeyState[ vkey ] = 0x80 if down else 0x00
+            self.__m_KeyState[ Const.VK_SHIFT ] = OR( self.__m_KeyState[ Const.VK_LSHIFT ], self.__m_KeyState[ Const.VK_RSHIFT ] )
 
-        elif( vkey == Const.VK_CONTROL or vkey == Const.VK_LCONTROL or vkey == Const.Const.Const.VK_NUMLOCK ):
-            keyboard_state[ vkey ] = 0x80 if down else 0x00
-            keyboard_state[ Const.VK_CONTROL ] = OR( keyboard_state[ Const.VK_LCONTROL ], keyboard_state[ Const.Const.Const.VK_NUMLOCK ] )
+        elif( vkey == Const.VK_CONTROL or vkey == Const.VK_LCONTROL or vkey == Const.VK_NUMLOCK ):
+            self.__m_KeyState[ vkey ] = 0x80 if down else 0x00
+            self.__m_KeyState[ Const.VK_CONTROL ] = OR( self.__m_KeyState[ Const.VK_LCONTROL ], self.__m_KeyState[ Const.VK_NUMLOCK ] )
 
         elif( vkey == Const.VK_NUMLOCK and not down ):
-            keyboard_state[ Const.VK_NUMLOCK ] = not keyboard_state[ Const.VK_NUMLOCK ]
+            self.__m_KeyState[ Const.VK_NUMLOCK ] = not self.__m_KeyState[ Const.VK_NUMLOCK ]
 
         elif( vkey == Const.VK_CAPITAL and not down ):
-            keyboard_state[ Const.VK_CAPITAL ] = not keyboard_state[ Const.VK_CAPITAL ]
+            self.__m_KeyState[ Const.VK_CAPITAL ] = not self.__m_KeyState[ Const.VK_CAPITAL ]
 
         elif( vkey == Const.VK_SCROLL and not down ):
-            keyboard_state[ Const.VK_SCROLL ] = not keyboard_state[ Const.VK_SCROLL ]
+            self.__m_KeyState[ Const.VK_SCROLL ] = not self.__m_KeyState[ Const.VK_SCROLL ]
 
 
 
@@ -226,3 +226,4 @@ class KeyLogger:
 
         elif(msg == Const.WM_KEYUP or msg == Const.WM_SYSKEYUP):
             self.__SetKeyState( vkey, 0 )
+
